e20de557d9523f9c968846f031e1c3d84c050146
your-name <you@gitee.com>
2026-08-29 03:04:54 +0800

    Mineradio文章封面更换为imagehub新封面图

--- script.js
+++ script.js
@@ -691,7 +691,7 @@
         <p>依托完全开源的底层代码逻辑，全程无强制弹窗广告，没有任何诱导点击的付费入口。与"音域回响"等项目联动，小红车（Wallpaper Engine）原生植入。</p>
       `,
       cta: '阅读全文',
-      image: 'images/mineradio.jpg'
+      image: 'https://s1.imagehub.cc/images/2026/08/29/17b9b97d7f2439155697329e475f0f26.png'
     }
   ];
 
